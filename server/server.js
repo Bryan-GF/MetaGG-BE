449cda500720bc9ac5b4f0cd2142e87b76add2d8
Chiboba <briangfelix@gmail.com>
2019-07-11 16:14:56 -0700

    API call for ranked data, returns hashtable built from response data

--- server/server.js
+++ server/server.js
@@ -18,4 +18,19 @@ server.post('/api/user/by-name', async (req,res) => {
     });
 });
 
+server.post('/api/user/by-name/ranked', async (req,res) => {
+    await request(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${req.body.summonerId}?api_key=${process.env.RIOT_KEY}`, function (error, response, body) {
+        if(error) {
+            res.status(500).json(error);
+        } else {
+            let jsonResponse = JSON.parse(body);
+            let rankedData = {}
+            for(let i = 0; i < jsonResponse.length; i++) {
+                rankedData[jsonResponse[i].queueType] = jsonResponse[i];
+            }
+            res.status(response.statusCode).json(rankedData);
+        }
+    });
+});
+
 module.exports = server;
